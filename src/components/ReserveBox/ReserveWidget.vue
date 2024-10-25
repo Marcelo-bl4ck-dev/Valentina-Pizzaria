86ecfaf08564dfc7998d75d6eb470084d2940aeb
Marcelo Lopes <marcelo.dev.sw@gmail.com>
2024-10-25 09:40:28 -0300

    Add reserve widget

--- src/components/ReserveBox/ReserveWidget.vue
+++ src/components/ReserveBox/ReserveWidget.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="align-start"> 
+    <div class="align-start">
         <iframe src="https://reservation-widget.tagme.com.br/group/66fd4ab81281e98ee28002f2" 
             frameborder="0" scrolling="auto" width="350" height="650px">
             Desculpe, seu navegador não suporta iframes.
